fa057ec683c655dffa56143816592a5e78fcfa39
haikujenny6 <haikujenny6@gmail.com>
2024-10-21 04:43:18 -0700

    Add files via upload
    Upload of image files, exported networks, html pages, miniscript code and images used in the module

---
+++ Q1.lua
@@ -0,0 +1,20 @@
+while 1
+if browser_was_button_click("D") then
+  browser_dispatch_successful_command("echo", "", "D")
+  browser_delete_absolute_div("A")
+  browser_delete_absolute_div("B")
+  browser_delete_absolute_div("C")
+  break
+else if browser_was_button_click("A") then
+    browser_dispatch_successful_command("echo", "", "A")
+    browser_delete_absolute_div("A")
+  else if browser_was_button_click("B") then
+    browser_dispatch_successful_command("echo", "", "B")
+    browser_delete_absolute_div("B")
+  else if browser_was_button_click("C") then
+    browser_dispatch_successful_command("echo", "", "C")
+    browser_delete_absolute_div("C")
+end if
+
+wait(0.1)
+end while
---
+++ Q2.lua
@@ -0,0 +1,20 @@
+while 1
+if browser_was_button_click("B") then
+  browser_dispatch_successful_command("echo", "", "B")
+  browser_delete_absolute_div("A")
+  browser_delete_absolute_div("C")
+  browser_delete_absolute_div("D")
+  break
+else if browser_was_button_click("A") then
+    browser_dispatch_successful_command("echo", "", "A")
+    browser_delete_absolute_div("A")
+  else if browser_was_button_click("C") then
+    browser_dispatch_successful_command("echo", "", "C")
+    browser_delete_absolute_div("C")
+  else if browser_was_button_click("D") then
+    browser_dispatch_successful_command("echo", "", "D")
+    browser_delete_absolute_div("D")
+end if
+
+wait(0.1)
+end while
